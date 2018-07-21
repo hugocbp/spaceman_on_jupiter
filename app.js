131f6965027384fea6af794ec913113727967fb0
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2018-07-20 20:35:57 -0700

    Add horizontal scrolling obstacles

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ const FPS = 60;
 const PLAYER_HEIGHT = 50;
 const PLAYER_WIDTH = 20;
 const CANVAS_BOTTOM = canvas.height;
+const CANVAS_RIGHT = canvas.width;
+const CANVAS_LEFT = 0;
 const THRUST_SPEED = 10;
 const GRAVITY_SPEED = 5;
 
+// OBSTACLES
+// Scrolling Obstacles - akw: 1045 Lab 11 - Summer 2018
+const OBSTACLES_PER_SEC = 2;
+const OBSTACLE_LENGTH = 50;
+const SPD_MIN = 1;
+const SPD_MAX = 10;
+
 let player;
+let obstacles = [];
 
 // Point Class - akw: 1045 Assignment 11 - Summer 2018
 class Point {
@@ -72,6 +82,31 @@ class Player {
   }
 }
 
+class Obstacle {
+  constructor(x, y, dx) {
+    this.pos = new Point(x, y);
+    this.dx = dx;
+  }
+  update() {
+    this.pos.x -= this.dx;
+  }
+  isVisible() {
+    if (this.pos.x < 0) {
+      return false;
+    }
+    return true;
+  }
+  draw(ctx) {
+    // draw the line onto the context
+    ctx.save();
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.pos.x, this.pos.y, 50, 20);
+
+    ctx.restore();
+  }
+}
+
 initGame();
 setInterval(tick, 1000 / FPS);
 
@@ -85,7 +120,13 @@ function initGame() {
 }
 
 function update() {
+  if (obstacles.length <= 5) {
+    generateObstacles();
+  }
+
   player.update();
+  obstacles.forEach(obstacle => obstacle.update());
+  obstacles = obstacles.filter(obstacle => obstacle.isVisible());
 }
 
 function drawEverything() {
@@ -93,8 +134,20 @@ function drawEverything() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   // TODO: Debug below
   // ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+  obstacles.forEach(obstacle => obstacle.draw(ctx));
   player.draw(ctx);
+}
+
+function generateObstacles() {
+  let randY = rand(0, CANVAS_BOTTOM);
+  let length = rand(0, 40);
+  let obstacle = new Obstacle(
+    canvas.width - OBSTACLE_LENGTH,
+    rand(0, canvas.height),
+    rand(SPD_MIN, SPD_MAX)
+  );
+
+  obstacles.push(obstacle);
 }
 
 // CONTROLS
@@ -112,3 +165,18 @@ window.addEventListener("keyup", e => {
     player.currentThrust = 0;
   }
 });
+
+/*
+		HELPERS
+*/
+function rand(min, max) {
+  if (min === undefined) {
+    min = 0;
+    max = 1;
+  } else if (max === undefined) {
+    max = min;
+    min = 0;
+  }
+
+  return Math.random() * (max - min) + min;
+}
